46b5f0b39695e34b3412e08e88c8668e89435278
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-06-08 19:33:08 +0300

    Create AppointmentRepository.java

---
+++ src/main/java/com/mahmoud/clinicmanagementsystem/repository/AppointmentRepository.java
@@ -0,0 +1,7 @@
+package com.mahmoud.clinicmanagementsystem.repository;
+
+import com.mahmoud.clinicmanagementsystem.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, String> {
+}

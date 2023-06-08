22a499339d8b205597fc3057bac4da833754dac7
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-06-08 20:01:54 +0300

    Create BaseHasRoleRequest.java

---
+++ src/main/java/com/mahmoud/clinicmanagementsystem/request/hasrole/BaseHasRoleRequest.java
@@ -0,0 +1,17 @@
+package com.mahmoud.clinicmanagementsystem.request.hasrole;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseHasRoleRequest {
+    private LocalDateTime timeFrom;
+    private LocalDateTime timeTo;
+    private String roleId;
+    private String employeeId;
+}

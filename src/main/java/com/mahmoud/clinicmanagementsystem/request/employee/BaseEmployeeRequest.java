93e61f82ab3cce21beff804db5eae92fca762a70
mahmoudfawzy9 <47506233+mahmoudfawzy9@users.noreply.github.com>
2023-06-08 20:00:21 +0300

    Create BaseEmployeeRequest.java

---
+++ src/main/java/com/mahmoud/clinicmanagementsystem/request/employee/BaseEmployeeRequest.java
@@ -0,0 +1,16 @@
+package com.mahmoud.clinicmanagementsystem.request.employee;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseEmployeeRequest {
+    private String username;
+    private String password;
+    private String email;
+
+}
